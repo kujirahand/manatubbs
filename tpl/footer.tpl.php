7dd64392f6ae2f9852071397dc0a23d823fec85b
kujirahand <web@kujirahand.com>
2016-09-08 07:42:30 +0900

    URL修正

--- tpl/footer.tpl.php
+++ tpl/footer.tpl.php
@@ -1,8 +1,10 @@
 <div id="footer">
-  <a href="http://d.aoikujira.com/labo/page/manatubbs"> manatubbs (c) kujirahand</a> / 
+  <a href="http://kujirahand.com/wiki/index.php?manatubbs">
+     manatubbs (c) kujirahand</a> / 
   <a href="<?php echo $script_name?>?m=rss">
   <img src="img/rss.gif" alt="RSS"/> RSS
   </a>
 </div>
 </body>
 </html>
+
